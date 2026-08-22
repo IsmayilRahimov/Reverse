3c80e286a8cf1637134c725bd9934290c060edb6
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-22 18:13:07 +0400

    Initial commit

---
+++ src/firstday/TernaryOperator.java
@@ -0,0 +1,19 @@
+package firstday;
+
+public class TernaryOperator {
+
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        String result = (num % 2 == 0) ? "Dogrudur" : "Yalnisdir";
+
+        System.out.println(result);
+
+
+        String result2 = (num - 5 == 0) ? "Dogrudur" : "Yalnisdir";
+        System.out.println(result2);
+
+    }
+}
