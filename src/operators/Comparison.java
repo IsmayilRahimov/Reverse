7fc01f151df086eb535a0247e585fd84fc84f24e
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-22 19:51:19 +0400

    Java Course reply

---
+++ src/operators/Comparison.java
@@ -0,0 +1,23 @@
+package operators;
+
+public class Comparison {
+
+
+    public static void main(String[] args) {
+
+
+        boolean b = true;
+
+
+        int a = 1;
+        int c = 2;
+
+        if (a == c) {
+            System.out.println(b);
+        } else {
+            System.out.println(false);
+        }
+
+
+    }
+}
