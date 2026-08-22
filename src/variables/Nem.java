a5a8e34b5ff2ca3e8a9496aaf435d236bb38244c
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-22 19:31:45 +0400

    .

---
+++ src/variables/Nem.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Nem {
+
+
+    public static void main(String[] args) {
+
+        String name = "Ismayil ";
+        int age = 19;
+        boolean gender = true;
+
+        System.out.println(name + " " + age + " " + gender);
+
+    }
+}
