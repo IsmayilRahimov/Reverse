3ab7136dea38b8a8b1f33a9c972cbc184916ae77
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-22 19:47:33 +0400

    Java Course reply

---
+++ src/operators/Assingment.java
@@ -0,0 +1,31 @@
+package operators;
+
+public class Assingment {
+
+
+    public static void main(String[] args) {
+
+
+        int a = 5;
+
+        a -= 3;
+        System.out.println(a);
+
+
+        int b = 5;
+
+        b += 3;
+        System.out.println(b);
+
+
+        int c = 5;
+
+        c /= 3;
+        System.out.println(c);
+
+
+        int d = 5;
+        d %= 3;
+        System.out.println(d);
+    }
+}
