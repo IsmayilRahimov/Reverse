c1ef41a3d55ebf3bfadbf46b0e7713b5edc3d676
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-23 16:37:46 +0400

    Java Course reply
    Switch reply course

---
+++ src/ElseIFElse/CaseDay.java
@@ -0,0 +1,23 @@
+package ElseIFElse;
+
+public class CaseDay {
+
+
+    static int day = 1;
+
+    public static void main(String[] args) {
+
+        switch (day) {
+            case 1:
+                System.out.println("Bazar ertesi:");
+                break;
+            case 2:
+                System.out.println("Cersenbe axsami:");
+                break;
+            case 3:
+                System.out.println("Cersenbe:");
+                break;
+        }
+    }
+
+}
