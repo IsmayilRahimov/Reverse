cba39ccdbc98c718dd7482f6334f7f6a1f86c049
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-23 16:17:17 +0400

    Java Course reply
    Operators reply

--- src/opera/InstanceOfExe.java
+++ src/opera/InstanceOfExe.java
@@ -1,4 +1,17 @@
 package opera;
 
 public class InstanceOfExe {
+
+    public static void main(String[] args) {
+        String name = "Ismayil";
+
+        System.out.println(name instanceof String);
+
+        Integer age = 10;
+        System.out.println(age instanceof Integer);
+
+
+
+
+    }
 }
