2e5bbe8cf9f99e5108061746f030b552cf5642d1
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-23 18:51:52 +0400

    Java Course reply
    Switch reply course

---
+++ src/DoForWhile/Intern.java
@@ -0,0 +1,22 @@
+package DoForWhile;
+
+public class Intern {
+
+    public static void main(String[] args) {
+
+        for (int a = 1; a <= 10; a++) {
+            System.out.println("a = " + a);
+        }
+
+
+        int b = 0;
+        do {
+
+            b++;
+            System.out.println(b);
+
+        } while (b >= 10);
+
+
+    }
+}
