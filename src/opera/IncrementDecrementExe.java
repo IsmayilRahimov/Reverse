fcb4d03547354377fcd0a95bb98e37a2a3666460
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-23 16:05:46 +0400

    Java Course reply
    Operators reply

--- src/opera/IncrementDecrementExe.java
+++ src/opera/IncrementDecrementExe.java
@@ -1,4 +1,14 @@
 package opera;
 
 public class IncrementDecrementExe {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a);
+
+    }
 }
