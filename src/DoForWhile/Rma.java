b501e90e6f0081fcc144faa31f4f0572fe86543a
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-08-23 17:37:12 +0400

    Java Course reply
    Switch reply course

---
+++ src/DoForWhile/Rma.java
@@ -0,0 +1,24 @@
+package DoForWhile;
+
+public class Rma {
+
+    public static void main(String[] args) {
+
+//        for (int i = 0; i <= 10; ++i) {
+//            System.out.println(i);
+//        }
+
+
+        int a = 5;
+//        while (a < 10) {
+//            a++;
+//            System.out.println(a);
+//        }
+
+        do {
+            a++;
+            System.out.println(a);
+        } while (a < 10);
+
+    }
+}
